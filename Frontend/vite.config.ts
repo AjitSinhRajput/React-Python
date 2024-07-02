070c432a1c9d0064cecbc090e2ad385d07c2907b
eonian/ajit <eonian_tfs1@hotmail.com>
2024-07-02 16:41:04 +0530

    commit from office

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.10.121", // Specify your custom IP address here
+    host: "192.168.0.184", // Specify your custom IP address here
     port: 3000, // Optional: Specify the port number
   },
 });
